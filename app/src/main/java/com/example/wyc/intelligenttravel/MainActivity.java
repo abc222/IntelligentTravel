c6b1604986b27e4ec6b268567c8cb8f3ad4864f2
WangYuchao <1569433461@qq.com>
2018-06-26 16:55:36 +0800

    显示定位蓝点

--- app/src/main/java/com/example/wyc/intelligenttravel/MainActivity.java
+++ app/src/main/java/com/example/wyc/intelligenttravel/MainActivity.java
@@ -2,18 +2,19 @@ package com.example.wyc.intelligenttravel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.util.Log;
 import com.amap.api.maps.AMap;
+import com.amap.api.maps.CameraUpdateFactory;
 import com.amap.api.maps.MapView;
-
-public class MainActivity extends AppCompatActivity {
+import com.amap.api.maps.model.MyLocationStyle;
+public class MainActivity extends AppCompatActivity{
 
     MapView mMapView = null;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         //获取地图控件引用
         mMapView = (MapView) findViewById(R.id.map);
@@ -23,6 +24,21 @@ public class MainActivity extends AppCompatActivity {
         if (aMap == null) {
             aMap = mMapView.getMap();
         }
+        //设置地图的放缩级别
+        aMap.moveCamera(CameraUpdateFactory.zoomTo(13));
+        MyLocationStyle myLocationStyle;
+        //初始化定位蓝点样式类
+        myLocationStyle = new MyLocationStyle();
+        //连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);
+        //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+        myLocationStyle.interval(2000);
+        //设置定位蓝点的Style
+        aMap.setMyLocationStyle(myLocationStyle);
+        //设置默认定位按钮是否显示，非必需设置。
+        aMap.getUiSettings().setMyLocationButtonEnabled(true);
+        // 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
+        aMap.setMyLocationEnabled(true);
     }
 
     @Override
@@ -49,5 +65,4 @@ public class MainActivity extends AppCompatActivity {
         //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
         mMapView.onSaveInstanceState(outState);
     }
-
 }
