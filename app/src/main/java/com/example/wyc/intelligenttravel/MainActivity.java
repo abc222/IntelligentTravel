0b3f2b3b44991b43923dc52dc3dd7555bda49e35
WangYuchao <1569433461@qq.com>
2018-06-27 09:30:16 +0800

    设置了动态申请定位权限，不用每次都去手机权限管理设置，但是效果不好，每次打开APP，都会显示出从北京到当前位置改变的过程，而且报错有权限被拒绝，待解决。

--- app/src/main/java/com/example/wyc/intelligenttravel/MainActivity.java
+++ app/src/main/java/com/example/wyc/intelligenttravel/MainActivity.java
@@ -1,15 +1,46 @@
 package com.example.wyc.intelligenttravel;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
+
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.CameraUpdateFactory;
 import com.amap.api.maps.MapView;
 import com.amap.api.maps.model.MyLocationStyle;
-public class MainActivity extends AppCompatActivity{
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
 
     MapView mMapView = null;
+    AMap aMap = null;
+
+    /**
+     * 需要进行检测的权限数组
+     */
+    protected String[] needPermissions = {
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+    //        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+    //        Manifest.permission.READ_EXTERNAL_STORAGE,
+    //        Manifest.permission.READ_PHONE_STATE
+    };
+    private static final int PERMISSON_REQUESTCODE = 0;
+    /**
+     * 判断是否需要检测，防止不停的弹框
+     */
+    private boolean isNeedCheck = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +51,7 @@ public class MainActivity extends AppCompatActivity{
         mMapView = (MapView) findViewById(R.id.map);
         //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
         mMapView.onCreate(savedInstanceState);
-        AMap aMap = null;
+
         if (aMap == null) {
             aMap = mMapView.getMap();
         }
@@ -39,7 +70,29 @@ public class MainActivity extends AppCompatActivity{
         aMap.getUiSettings().setMyLocationButtonEnabled(true);
         // 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
         aMap.setMyLocationEnabled(true);
+//        //动态获取定位权限
+//        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+//                != PackageManager.PERMISSION_GRANTED) {
+//            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+//        } else {
+//            aMap.setMyLocationEnabled(true);
+//        }
     }
+
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        switch (requestCode) {
+//            case 1:
+//                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                    aMap.setMyLocationEnabled(true);
+//                } else {
+//                    Toast.makeText(MainActivity.this, "缺少定位权限，无法完成定位~", Toast.LENGTH_SHORT).show();
+//                }
+//                break;
+//            default:
+//        }
+//    }
+
 
     @Override
     protected void onDestroy() {
@@ -47,18 +100,119 @@ public class MainActivity extends AppCompatActivity{
         //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
         mMapView.onDestroy();
     }
+
+    //----------以下动态获取权限---------
     @Override
     protected void onResume() {
         super.onResume();
+        if (isNeedCheck) {
+            checkPermissions(needPermissions);
+        }
         //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
         mMapView.onResume();
     }
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
+//        mMapView.onResume();
+//    }
+
+    /**
+     * 检查权限
+     */
+    private void checkPermissions(String... permissions) {
+        //获取权限列表
+        List needRequestPermissonList = findDeniedPermissions(permissions);
+        if (null != needRequestPermissonList && needRequestPermissonList.size() > 0) {
+            //list.toarray将集合转化为数组
+            ActivityCompat.requestPermissions(this,
+                    (String [])needRequestPermissonList.toArray(new String[needRequestPermissonList.size()]),
+                    PERMISSON_REQUESTCODE);
+        }
+    }
+
+    /**
+     * 获取权限集中需要申请权限的列表
+     *
+     * @param permissions
+     * @return
+     * @since 2.5.0
+     */
+    private List findDeniedPermissions(String[] permissions) {
+        List needRequestPermissonList = new ArrayList();
+        //for (循环变量类型 循环变量名称 : 要被遍历的对象)
+        for (String perm : permissions) {
+            if (ContextCompat.checkSelfPermission(this,
+                    perm) != PackageManager.PERMISSION_GRANTED
+                    || ActivityCompat.shouldShowRequestPermissionRationale(
+                    this, perm)) {
+                needRequestPermissonList.add(perm);
+            }
+        }
+        return needRequestPermissonList;
+    }
+
+    /**
+     * 检测是否说有的权限都已经授权
+     *
+     * @param grantResults
+     * @return
+     */
+    private boolean verifyPermissions(int[] grantResults) {
+        for (int result : grantResults) {
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] paramArrayOfInt) {
+        if (requestCode == PERMISSON_REQUESTCODE) {
+            if (!verifyPermissions(paramArrayOfInt)) {//没有授权
+                //showMissingPermissionDialog();//显示提示信息
+                Toast.makeText(MainActivity.this, "缺少权限，无法完成定位~", Toast.LENGTH_SHORT).show();
+                isNeedCheck = false;
+            }
+        }
+    }
+    /**
+     * 显示提示信息
+     *
+     * @since 2.5.0
+     */
+//    private void showMissingPermissionDialog() {
+//        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//        builder.setTitle(R.string.notifyTitle);
+//        builder.setMessage(R.string.notifyMsg);
+//// 拒绝, 退出应用
+//        builder.setNegativeButton(R.string.cancel,
+//                new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        finish();
+//                    }
+//                });
+//        builder.setPositiveButton(R.string.setting,
+//                new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        startAppSettings();
+//                    }
+//                });
+//        builder.setCancelable(false);
+//        builder.show();
+//    }
+
     @Override
     protected void onPause() {
         super.onPause();
         //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
         mMapView.onPause();
     }
+
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
